527f908c3c9d6a9b68c9106cf9f5e04dded7a895
orddreamer <roman.snitsarenko@gmail.com>
2024-12-26 20:46:11 +0100

    Init

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://f042-83-168-77-34.ngrok-free.app/'; // Змінити на вашу адресу сервера
+const SERVER_URL = 'https://eb75-83-168-77-34.ngrok-free.app/'; // Змінити на вашу адресу сервера
 
 const tg = window.Telegram.WebApp;
 tg.expand(); // Розгортає міні-додаток на весь екран
